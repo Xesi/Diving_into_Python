d78c3480bda066de8cb74bd99db41872470ae814
Georgiy Ulanov <georgiyulanov@ya.ru>
2020-10-27 11:25:40 +0300

    week3 # T1 - solution.py

---
+++ week3/solution.py
@@ -0,0 +1,12 @@
+class FileReader:
+
+    def __init__(self, path):
+        self._path = path
+
+    def read(self):
+        try:
+            with open(self._path) as file:
+                return file.read()
+
+        except FileNotFoundError:
+            return str()
